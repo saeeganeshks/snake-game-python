58862663680dad8fc328ee43f93143d13415d1c3
Saee Ganesh K S <saeeganeshks@gmail.com>
2021-03-16 17:22:51 +0530

    added clear boundary

--- SnakeGame.py
+++ SnakeGame.py
@@ -10,11 +10,13 @@ black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
+blue2 = (0, 0, 139, 255)
+
+bound = 50 
+dis_width = 500
+dis_height = 500
  
-dis_width = 600
-dis_height = 400
- 
-dis = pygame.display.set_mode((dis_width, dis_height))
+dis = pygame.display.set_mode((dis_width+bound,dis_height+bound))
 pygame.display.set_caption('Snake Game')
  
 clock = pygame.time.Clock()
@@ -23,23 +25,22 @@ snake_block = 10
 snake_speed = 15
  
 font_style = pygame.font.SysFont("Helvetica", 25)
-score_font = pygame.font.SysFont("Helvetica", 35)
+score_font = pygame.font.SysFont("Times", 35)
  
  
-def Your_score(score):
+def Score(score):
     value = score_font.render("Your Score: " + str(score), True, yellow)
     dis.blit(value, [0, 0])
  
  
- 
-def our_snake(snake_block, snake_list):
+def Snake(snake_block, snake_list):
     for x in snake_list:
         pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
  
  
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+    dis.blit(mesg, [bound, dis_height/2])
  
  
 def gameLoop():
@@ -55,18 +56,18 @@ def gameLoop():
     snake_List = []
     Length_of_snake = 1
  
-    dotx1 = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    doty1 = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+    dotx1 = round(random.randrange(bound, dis_width - snake_block) / 10.0) * 10.0
+    doty1 = round(random.randrange(bound, dis_height - snake_block) / 10.0) * 10.0
 
-    dotx2 = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    doty2 = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+    dotx2 = round(random.randrange(bound, dis_width - snake_block) / 10.0) * 10.0
+    doty2 = round(random.randrange(bound, dis_height - snake_block) / 10.0) * 10.0
  
     while not game_over:
  
         while game_close == True:
-            dis.fill(blue)
+            dis.fill(blue2)
             message("You Lost! Press C-Play Again or Q-Quit", red)
-            Your_score(Length_of_snake - 1)
+            Score(Length_of_snake - 1)
             pygame.display.update()
  
             for event in pygame.event.get():
@@ -94,11 +95,12 @@ def gameLoop():
                     y1_change = snake_block
                     x1_change = 0
  
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+        if x1 >= dis_width or x1 < bound or y1 >= dis_height or y1 < bound:
             game_close = True
         x1 += x1_change
         y1 += y1_change
-        dis.fill(blue)
+        dis.fill(blue2)
+        pygame.draw.rect(dis,blue,(bound,bound,dis_width-bound,dis_height-bound))
         pygame.draw.rect(dis, green, [dotx1, doty1, snake_block, snake_block])
         pygame.draw.rect(dis, green, [dotx2, doty2, snake_block, snake_block])
         snake_Head = []
@@ -112,18 +114,18 @@ def gameLoop():
             if x == snake_Head:
                 game_close = True
  
-        our_snake(snake_block, snake_List)
-        Your_score(Length_of_snake - 1)
+        Snake(snake_block, snake_List)
+        Score(Length_of_snake - 1)
  
         pygame.display.update()
  
-        if x1 == dotx1 and y1 == doty2:
-            dotx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            doty = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+        if x1 == dotx1 and y1 == doty1:
+            dotx1 = round(random.randrange(bound, dis_width - snake_block) / 10.0) * 10.0
+            doty1 = round(random.randrange(bound, dis_height - snake_block) / 10.0) * 10.0
             Length_of_snake += 1
         elif x1 == dotx2 and y1 == doty2:
-            dotx2 = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            doty2 = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            dotx2 = round(random.randrange(bound, dis_width - snake_block) / 10.0) * 10.0
+            doty2 = round(random.randrange(bound, dis_height - snake_block) / 10.0) * 10.0
             Length_of_snake += 1
  
         clock.tick(snake_speed)
